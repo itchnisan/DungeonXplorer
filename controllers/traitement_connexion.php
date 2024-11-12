f43a59b54fa3ba98bcf4da8720b00a8a44a742fb
fortin233 <lukas.fortin@etu.unicaen.fr>
2024-11-12 17:56:21 +0100

    V2 qui marche

--- controllers/traitement_connexion.php
+++ controllers/traitement_connexion.php
@@ -1,91 +1,105 @@
 <?php
-
 session_start();
 
-	function afficherObj($obj)
-	{
-		echo "<PRE>";
-		print_r($obj);
-		echo "</PRE>";
-	}
+function afficherObj($obj)
+{
+    echo "<PRE>";
+    print_r($obj);
+    echo "</PRE>";
+}
 
-	include_once "../models/connexionPDO.php";
+include_once "../models/connexionPDO.php";
 
-    $_SESSION['loggedin'] = false;
-	$_SESSION['isAdmin'] = false;
-	$_SESSION['numero'];
-	$_SESSION['courriel'];
+$_SESSION['loggedin'] = false;
+$_SESSION['isAdmin'] = false;
+$_SESSION['numero'] = null;
+$_SESSION['courriel'] = null;
 
-	function connexion()
-    {	
+function connexion($mysqlClient)
+{
+    $erreur = 0;
 
-		$erreur = 0;
+    if (!empty($_POST["courriel"]) && !empty($_POST["mdp"])) {
+        $courriel = $_POST["courriel"];
+        $mdp = $_POST["mdp"];
+    } else {
+        $erreur = 1; // Fields are not filled
+    }
 
-		if(!empty($_POST["courriel"]) and (filter_var($_POST["courriel"], FILTER_VALIDATE_EMAIL)) and !empty($_POST["mdp"]))
-		{
-			$courriel = $_POST["courriel"];
-			$mdp = $_POST["mdp"];
-		}
-		else
-		{
-			$erreur = 1; // n'a pas renseigner tout les champs
-		}
+    if ($erreur == 0) {
+        $sql = "SELECT user_mdp FROM USER WHERE user_mail = :courriel";
+        $cur = preparerRequetePDO($mysqlClient, $sql);
+        ajouterParamPDO($cur, ':courriel', $courriel, 'texte');
+        $donnee = [];
+        $res = LireDonneesPDOPreparee($cur, $donnee);
 
-		if($erreur == 0)
-		{
-			$sql = "SELECT hero_id FROM USER WHERE user_mail = '".$courriel."'";
-			$donnee;
-			$existe = LireDonneesPDO2($mysqlClient,$sql,$donnee);
-			if($existe == 0)
-			{
-				$erreur = 2; // existe pas dans la base
-			} 
-			else
-			{
-				$sql = "SELECT user_mdp FROM USER WHERE user_mail = '".$courriel."'";
-				$donnee;
-				$res = LireDonneesPDO2($mysqlClient,$sql,$donnee);
-				if (isset($donnee[0])){
-					// Comparez les mots de passe
-					if ($donnee[0]["PER_MDP"] != $mdp) 
-					{
-						$erreur = 3; // Le mot de passe n'est pas bon
-					}
-					else
-					{
-						$sql = "SELECT hero_id  FROM USER WHERE user_mail = '".$courriel."'";
-						$donnee;
-						$res = LireDonneesPDO2($c,$sql,$donnee);
-						$_SESSION['numero'] = $donnee[0]["hero_id"];
-						$_SESSION['courriel'] = $courriel;
+        if ($res == 0) {
+            $erreur = 2; // User doesn't exist
+        } else {
+            if (isset($donnee[0])) {
+                if ($mdp != $donnee[0]["user_mdp"]) {
+                    $erreur = 3; // Incorrect password
+                } else {
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['courriel'] = $courriel;
+                    echo $courriel;
+                    echo $_SESSION['courriel'];
 
-						$sql = "SELECT admin_id FROM ADMINS WHERE user_mail = '".$courriel."'";
-			            $donnee;
-                        $existe = LireDonneesPDO2($mysqlClient,$sql,$donnee);
-                        if($existe == 1)
-                        {
-                            $_SESSION['loggedin'] = true; // l'utilisateur est un administrateur
-                        } 
-					}
-				}
-			}
-		}
-		
-		switch($erreur)
-		{
-			case 0:
-				echo $_SESSION['loggedin'];
-                break;
-			default:
-				echo "<script type='text/javascript'>
-					alert('Courriel ou MDP incorrect');
-					window.location.href = '../index.php';
-				</script>";
-                break;
-		}
-	}
+                    $sql = "SELECT hero_id FROM USER WHERE user_mail = :courriel";
+                    $cur = preparerRequetePDO($mysqlClient, $sql);
+                    ajouterParamPDO($cur, ':courriel', $courriel, 'texte');
+                    $donnee = [];
+                    $res = LireDonneesPDOPreparee($cur, $donnee);
+                    if ($res > 0) {
+                        $_SESSION['numero'] = $donnee[0]["hero_id"];
+                    }
 
-	if($mysqlClient){
-		connexion($mysqlClient);
-	}
+                    // Check if user is an admin (if needed)
+                    /* $sql = "SELECT admin_id FROM ADMINS WHERE user_mail = :courriel";
+                    $cur = preparerRequetePDO($mysqlClient, $sql);
+                    ajouterParamPDO($cur, ':courriel', $courriel, 'texte');
+                    $donnee = [];
+                    $res = LireDonneesPDOPreparee($cur, $donnee);
+                    if ($res > 0) {
+                        $_SESSION['isAdmin'] = true;
+                    } */
+                }
+            }
+        }
+    }
+
+    switch ($erreur) {
+        case 0:
+            // Successfully logged in
+            echo "ok";
+            echo $_SESSION['loggedin'];
+            echo $_SESSION['isAdmin'];
+            echo $_SESSION['numero'];
+            echo $_SESSION['courriel'];
+            break;
+
+        case 1:
+            echo "Please fill in all fields.";
+            break;
+
+        case 2:
+            echo "User does not exist in the database.";
+            break;
+
+        case 3:
+            echo "Incorrect password.";
+            break;
+
+        default:
+            echo "<script type='text/javascript'>
+                    alert('Email or password is incorrect');
+                    window.location.href = '../index.php';
+                </script>";
+            break;
+    }
+}
+
+if ($mysqlClient) {
+    connexion($mysqlClient);
+}
 ?>
