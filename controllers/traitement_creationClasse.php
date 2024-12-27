f4ad70b114a0c2d48184174c2538eb1d78a5c72e
Julien Moisson <moisson232@unicaen.fr>
2024-12-27 10:18:09 +0100

    reparation de la creation d'un hero

--- controllers/traitement_creationClasse.php
+++ controllers/traitement_creationClasse.php
@@ -58,8 +58,12 @@ function creerClasse($mysqlClient) {
 
     //modifier bdd pour qu'elle associe directement des spell ,armes et armures aux classes
 
-    $sql = "INSERT INTO Hero (user_id,hero_name, class_id, hero_image, hero_biography,hero_pv, hero_mana, hero_strength,hero_initiative,hero_armor, primary_weapon, secondary_weapon, shield, spell_list, xp, current_level) 
-    VALUES (:user_id, :nom, :class_id, :image, :biography,:pv, :mana, :strength,:initiative,:armor, :primary_weapon, :secondary_weapon, :shield, :spell_list, :xp, :current_level)";
+    // $sql = "INSERT INTO Hero (user_id,hero_name, class_id, hero_image, hero_biography,hero_pv, hero_mana, hero_strength,hero_initiative,hero_armor, primary_weapon, secondary_weapon, shield, spell_list, xp, current_level) 
+    // VALUES (:user_id, :nom, :class_id, :image, :biography,:pv, :mana, :strength,:initiative,:armor, :primary_weapon, :secondary_weapon, :shield, :spell_list, :xp, :current_level)";
+
+    $sql = "INSERT INTO Hero (user_id,hero_name, class_id, hero_image, hero_biography,hero_pv, hero_mana, hero_strength,hero_initiative, hero_xp, current_level) 
+    VALUES (:user_id, :nom, :class_id, :image, :biography,:pv, :mana, :strength,:initiative,:xp, :current_level)";
+
 
     $cur = preparerRequetePDO($mysqlClient, $sql);
 
@@ -84,11 +88,11 @@ function creerClasse($mysqlClient) {
     ajouterParamPDO($cur, ':mana', $heroMana);
     ajouterParamPDO($cur, ':strength', $heroStrength);
     ajouterParamPDO($cur, ':initiative', $heroInitiative);
-    ajouterParamPDO($cur, ':armor', $heroArmor);
-    ajouterParamPDO($cur, ':primary_weapon', $heroPrimary);
-    ajouterParamPDO($cur, ':secondary_weapon', $heroSecondary);
-    ajouterParamPDO($cur, ':shield', $heroShield);
-    ajouterParamPDO($cur, ':spell_list', $heroSpellList);
+    // ajouterParamPDO($cur, ':armor', $heroArmor);
+    // ajouterParamPDO($cur, ':primary_weapon', $heroPrimary);
+    // ajouterParamPDO($cur, ':secondary_weapon', $heroSecondary);
+    //ajouterParamPDO($cur, ':shield', $heroShield);
+    //ajouterParamPDO($cur, ':spell_list', $heroSpellList);
     ajouterParamPDO($cur, ':xp', $heroXp);
     ajouterParamPDO($cur, ':current_level', $heroCurrentLevel);
 
